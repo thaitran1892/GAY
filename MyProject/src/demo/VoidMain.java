c2abc3c66289339c926e993ef2b55a807388e4d3
thaitran1892 <thai_tran1892@yahoo.com>
2012-10-18 22:43:42 +0700

    Update MyProject/src/demo/VoidMain.java

--- MyProject/src/demo/VoidMain.java
+++ MyProject/src/demo/VoidMain.java
@@ -5,7 +5,7 @@ public class VoidMain
 	public static void main (String[] args)
 	{
 		System.out.println("This is my project");
-		System.out.println("Hello");
+		System.out.println("Hello friends");
 	}
 	
 }
